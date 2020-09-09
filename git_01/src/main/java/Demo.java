845d37bb2e0d1b14d0a61311512ef2ddae4d16f6
chenwei2000 <952623502@qq.com>
2020-09-09 16:37:42 +0800

    第一次修改

--- git_01/src/main/java/Demo.java
+++ git_01/src/main/java/Demo.java
@@ -3,5 +3,15 @@ public class Demo {
     public void hello(){
         System.out.println("hello");
         System.out.println("hellww!!");
+
+
+
     }
+
+    public void demo(){
+
+        System.out.println("你奶奶个气门芯");
+
+    }
+
 }
