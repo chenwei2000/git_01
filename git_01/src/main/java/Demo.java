a720f1e8270f78d88341206c4842cef437fe72fc
chenwei2000 <952623502@qq.com>
2020-09-09 16:38:55 +0800

    Revert "第一次修改"
    This reverts commit 845d37bb

--- git_01/src/main/java/Demo.java
+++ git_01/src/main/java/Demo.java
@@ -3,15 +3,5 @@ public class Demo {
     public void hello(){
         System.out.println("hello");
         System.out.println("hellww!!");
-
-
-
     }
-
-    public void demo(){
-
-        System.out.println("你奶奶个气门芯");
-
-    }
-
 }
